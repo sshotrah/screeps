f9948a5e56aca12874cd49e3d5f25d582455a790
Camille Meunier <cmeunier@e-magineurs.com>
2017-08-09 16:27:11 +0200

    tutoriel 3

--- machine/main.js
+++ machine/main.js
@@ -1,1 +1,22 @@
-teste
+//Game.spawns['Spawn1'].createCreep( [WORK, CARRY, MOVE], 'Harvester1' );
+
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+
+module.exports.loop = function () {
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
--- machine/test.js
+++
@@ -1,4 +0,0 @@
-function test(){
-
-var u = 'test';
-}
