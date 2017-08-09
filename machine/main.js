3772573fbeb3d9b1e5a2889ec3ceaa127f36376d
Camille Meunier <cmeunier@e-magineurs.com>
2017-08-09 17:11:36 +0200

    Game1

--- machine/main.js
+++ machine/main.js
@@ -29,8 +29,7 @@ module.exports.loop = function () {
         }
     }
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
-    
+   
     if(harvesters.length < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
