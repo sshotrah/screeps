aaf099dc8d4c4deeaa39fb0924cb2121869130f9
Camille Meunier <cmeunier@e-magineurs.com>
2017-08-10 19:16:07 +0200

    soldier boucle

--- machine/role.soldier.js
+++ machine/role.soldier.js
@@ -2,8 +2,19 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.pos != Game.flags.Flag1.pos ){
-            creep.moveTo(Game.flags.Flag1);
+        var flags = Game.flags;
+        if(!creep.memory.action){
+            creep.memory.action = flags[0];
+        }
+        for(var i = 0; i < flags.length; i++){
+            if(creep.pos != creep.memory.action.pos ){
+                creep.moveTo(creep.memory.action);
+            }else{
+                if(creep.memory.action == flags[i]){
+                    var n = (i < flags.length)? i+1: 0;
+                    creep.memory.action = flags[n];
+                }
+            }
         }
     }
 };
